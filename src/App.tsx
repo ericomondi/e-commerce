a4f009d895d9ca5b572153d62190d59bf7f8f97a
wycliffe <ericoochieng456@gmail.com>
2025-05-15 12:55:47 +0300

    feat: add ToastContainer from react-toastify for notifications

--- src/App.tsx
+++ src/App.tsx
@@ -16,28 +16,41 @@ import OrderDetails from "./pages/OrderDetails";
 import OrderTracking from "./pages/OrderTracking";
 import OrdersOverview from "./pages/OrdersOverview";
 import ProductList from "./components/TestProductTable";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
+        <ToastContainer
+          position="top-right"
+          autoClose={1000}
+          hideProgressBar={true}
+          newestOnTop={true}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          progressClassName="toast-progress-bar"
+        />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           {/* <Route element={<ProtectedRoute />}> */}
-            <Route element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/store" element={<ProductCards />} />
-              <Route path="/shopping-cart" element={<ShoppingCart />} />
-              <Route path="/checkout" element={<Checkout />} />
-              <Route path="/order-summary" element={<OrderSummary />} />
-              <Route path="/order-confirmation" element={<OrderConfirmation />} />
-              <Route path="/order-tracking" element={<OrderTracking />} />
-              <Route path="/order-details" element={<OrderDetails />} />
-              <Route path="/orders-overview" element={<OrdersOverview />} />
-              <Route path="/test-products" element={<ProductList />} />
-            </Route>
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/store" element={<ProductCards />} />
+            <Route path="/shopping-cart" element={<ShoppingCart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/order-summary" element={<OrderSummary />} />
+            <Route path="/order-confirmation" element={<OrderConfirmation />} />
+            <Route path="/order-tracking" element={<OrderTracking />} />
+            <Route path="/order-details" element={<OrderDetails />} />
+            <Route path="/orders-overview" element={<OrdersOverview />} />
+            <Route path="/test-products" element={<ProductList />} />
+          </Route>
           {/* </Route> */}
         </Routes>
       </Router>
