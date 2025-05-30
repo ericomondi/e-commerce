da3f0d09f79055ee9cf0513f789b41752297870e
wycliffe <ericoochieng456@gmail.com>
2025-05-30 20:37:12 +0300

    feat: update Payment component to display formatted subtotal and delivery fee

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../cart/formatCurrency";
 
 const Payment: React.FC = () => {
+  const {total, deliveryFee, subtotal} = useShoppingCart();
   return (
    <section className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
   <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
@@ -61,28 +64,22 @@ const Payment: React.FC = () => {
             <div className="space-y-2">
               <dl className="flex items-center justify-between gap-4">
                 <dt className="text-base font-normal text-gray-500 dark:text-gray-400">Original price</dt>
-                <dd className="text-base font-medium text-gray-900 dark:text-white">$6,592.00</dd>
+                <dd className="text-base font-medium text-gray-900 dark:text-white">{formatCurrency(subtotal)}</dd>
               </dl>
 
-              <dl className="flex items-center justify-between gap-4">
-                <dt className="text-base font-normal text-gray-500 dark:text-gray-400">Savings</dt>
-                <dd className="text-base font-medium text-green-500">-$299.00</dd>
-              </dl>
+            
 
               <dl className="flex items-center justify-between gap-4">
-                <dt className="text-base font-normal text-gray-500 dark:text-gray-400">Store Pickup</dt>
-                <dd className="text-base font-medium text-gray-900 dark:text-white">$99</dd>
+                <dt className="text-base font-normal text-gray-500 dark:text-gray-400">Delivery Fee</dt>
+                <dd className="text-base font-medium text-gray-900 dark:text-white">{formatCurrency(deliveryFee)}</dd>
               </dl>
 
-              <dl className="flex items-center justify-between gap-4">
-                <dt className="text-base font-normal text-gray-500 dark:text-gray-400">Tax</dt>
-                <dd className="text-base font-medium text-gray-900 dark:text-white">$799</dd>
-              </dl>
+             
             </div>
 
             <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
               <dt className="text-base font-bold text-gray-900 dark:text-white">Total</dt>
-              <dd className="text-base font-bold text-gray-900 dark:text-white">$7,191.00</dd>
+              <dd className="text-base font-bold text-gray-900 dark:text-white">{formatCurrency(total)}</dd>
             </dl>
           </div>
 
